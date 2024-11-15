f5565ce0859b648e765c55c1f2e9c570d49411a6
doding2 <dragon041414@gmail.com>
2024-11-15 11:03:31 +0900

    Update: Add MAP@k Evaluation to user-based filtering method

--- RecommendingSystem_userBase.py
+++ RecommendingSystem_userBase.py
@@ -2,14 +2,15 @@ import pandas as pd
 from surprise import Dataset, Reader, KNNBasic
 from surprise import accuracy
 import random
+import numpy as np
 
 # 데이터셋 로드
-data_path = "merged_data2.csv"  
+data_path = "merged_data2.csv"
 full_data = pd.read_csv(data_path)  # CSV 파일을 pandas DataFrame으로 읽기
 
 # 10000개의 샘플을 랜덤으로 추출 (train dataset)
 # 정확도를 올리기 위해 샘플 개수를 1만에서 3만으로 증가시킴
-#sampled_data = full_data.sample(n=30000, random_state=0)  # random_state=0를 통해 일관된 테스트를 거쳤다는 것을 보장
+# sampled_data = full_data.sample(n=30000, random_state=0)  # random_state=0를 통해 일관된 테스트를 거쳤다는 것을 보장
 sampled_data = full_data.sample(n=30000)
 # Reader 클래스는 데이터의 평점 범위를 설정하는데 사용됨
 # 여기서는 평점 범위가 1에서 5로 설정됨
@@ -72,19 +73,71 @@ random_user = random.choice(test_data['user_id'].unique())
 print(f"추천을 위한 랜덤 사용자 ID: {random_user}")
 
 # 2. 해당 사용자가 평점을 매긴 아이템 목록을 추출
-user_ratings = sampled_data[sampled_data['user_id'] == random_user]
-rated_items = user_ratings['business_id'].unique()
+user_ratings = test_data[test_data['user_id'] == random_user]
+rated_items = user_ratings['name'].unique()
 
 # 3. 사용자가 평점을 매기지 않은 아이템 목록을 추출
-unrated_items = sampled_data[~sampled_data['business_id'].isin(rated_items)]['business_id'].unique()
+# unrated_items = test_data[~test_data['name'].isin(rated_items)]['name'].unique()
+
+# 3-1. 사용자가 평점을 매긴 아이템을 포함한 모든 아이템 목록을 추출해서 대신 사용
+unrated_items = test_data['name'].unique()
 
 # 4. 사용자가 평점 매지 않은 아이템에 대해 예측 평점 계산
 predicted_ratings = [(item, algo.predict(random_user, item).est) for item in unrated_items]
 
-# 5. 예측 평점이 높은 순으로 정렬하여 상위 3개의 추천 아이템 추출
-recommended_items = sorted(predicted_ratings, key=lambda x: x[1], reverse=True)[:3]
+# 5. 예측 평점이 높은 순으로 정렬하여 상위 k개의 추천 아이템 추출
+k = 5
+recommended_items = sorted(predicted_ratings, key=lambda x: x[1], reverse=True)[:k]
 
 # 6. 추천된 아이템 출력
 print("추천 아이템:")
 for item, rating in recommended_items:
     print(f"아이템 ID: {item}, 예측 평점: {rating}")
+
+# 7. 랜덤한 한 유저에 대한 rank 평가
+print('\nRank 평가')
+
+# precision@k
+true_positive = sum((item[0] in user_ratings.index for item in recommended_items))
+precision = true_positive / k
+print(f'precision@{k} of user {random_user}: {precision}')
+
+# recall@k
+recall = true_positive / len(user_ratings)
+print(f'recall@{k} of user {random_user}: {recall}')
+
+# 8. 모든 유저에 대한 rank 평가
+
+# Mean Average Precision@k
+test_df = test_data.drop_duplicates(subset=['user_id'])  # make user ids unique
+test_user_ids = test_df['user_id']
+average_precision_sum = 0
+
+for test_user_id in test_user_ids:
+    user_ratings = test_data[test_data['user_id'] == test_user_id]
+    unshaped_rated_items = user_ratings['name'].unique()
+    rated_items = []
+    for item in unshaped_rated_items:
+        rated_items += item
+
+    all_items = test_data['name'].unique()
+    predicted_ratings = [(item, algo.predict(random_user, item).est) for item in all_items]
+
+    recommended_items = sorted(predicted_ratings, key=lambda x: x[1], reverse=True)[:k]
+
+    # calculate precision in range 1 to k
+    precision_sum = 0
+    for k_i in range(1, k + 1):
+        # calculate Average Precision
+        # if item is rated by user, relevance is 1 and otherwise 0
+        # 0 make Average Precision to 0 so, calculate only when relevance is 1
+        if recommended_items[k_i - 1][0] in rated_items:
+            true_positive_of_i = sum((item in rated_items for item in recommended_items[:k_i]))
+            precision_of_i = true_positive_of_i / k_i
+            precision_sum += precision_of_i
+
+    average_precision = precision_sum / len(rated_items)
+    average_precision_sum += average_precision
+
+mean_average_precision = average_precision_sum / len(test_user_ids)
+print(f'mean average precision@{k}: {mean_average_precision}')
